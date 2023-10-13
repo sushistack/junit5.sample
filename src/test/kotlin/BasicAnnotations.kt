94e5be7da90eeb6382701927f52f7a714d4d6df4
shushistack <kjm71712@naver.com>
2023-10-14 00:20:11 +0900

    add BeforeEach

--- src/test/kotlin/BasicAnnotations.kt
+++ src/test/kotlin/BasicAnnotations.kt
@@ -1,9 +1,18 @@
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class BasicAnnotations {
+    lateinit var name: String
+
+    @BeforeEach
+    fun setup() {
+        name = "test"
+    }
+
     @Test
     @DisplayName(value = "디스플레이 네임 테스트.")
     fun displayNameTest() {
+        println(name)
     }
 }
