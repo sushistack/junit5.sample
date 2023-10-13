6feb5d3e0c88343669cc56c83c1554b9572fe663
shushistack <kjm71712@naver.com>
2023-10-14 00:16:04 +0900

    add BasicAnnotations class

---
+++ src/test/kotlin/BasicAnnotations.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class BasicAnnotations {
+    @Test
+    @DisplayName(value = "디스플레이 네임 테스트.")
+    fun displayNameTest() {
+    }
+}
