ce3f51576d6b5f469969f6b37f7dde84d2780a27
shushistack <kjm71712@naver.com>
2023-10-14 00:25:31 +0900

    add BeforeAll

--- src/test/kotlin/BasicAnnotations.kt
+++ src/test/kotlin/BasicAnnotations.kt
@@ -1,8 +1,18 @@
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class BasicAnnotations {
+    companion object {
+        var age = 1
+        @JvmStatic
+        @BeforeAll
+        fun setupOnce() {
+            age = 2
+        }
+    }
+
     lateinit var name: String
 
     @BeforeEach
@@ -14,5 +24,6 @@ class BasicAnnotations {
     @DisplayName(value = "디스플레이 네임 테스트.")
     fun displayNameTest() {
         println(name)
+        println(age)
     }
 }
