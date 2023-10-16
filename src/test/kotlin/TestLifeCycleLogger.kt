7eb89cbf82cea2f8de4989b7fb1267467b8c2593
shushistack <kjm71712@naver.com>
2023-10-16 21:25:30 +0900

    add Lifecycle PER_CLASS case

--- src/test/kotlin/TestLifeCycleLogger.kt
+++ src/test/kotlin/TestLifeCycleLogger.kt
@@ -2,33 +2,27 @@
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.junit.jupiter.api.*
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TestLifeCycleLogger {
+    private val log = KotlinLogging.logger {}
+    private lateinit var sharedVariable: String
 
-    companion object {
-        val log = KotlinLogging.logger {}
-
-        @JvmStatic
-        @BeforeAll
-        fun beforeAllTests() {
-            log.info { "Before all tests" }
-        }
-
-        @JvmStatic
-        @AfterAll
-        fun afterAllTests() {
-            log.info {"After all tests" }
-        }
+    // PER_CLASS 일 때, 인스턴스 메서드로 사용할 수 있음
+    @BeforeAll
+    fun beforeAllTests() {
+        log.info { "Before all tests" }
+        sharedVariable = "I'm Shared Variable !!"
     }
 
+    // PER_CLASS 일 때, 인스턴스 메서드로 사용할 수 있음
+    @AfterAll
+    fun afterAllTests() {
+        log.info {"After all tests" }
+    }
 
     @BeforeEach
     fun beforeEachTest(testInfo: TestInfo) {
-        log.info {
-            String.format(
-                "About to execute [%s]",
-                testInfo.displayName
-            )
-        }
+        log.info { String.format("About to execute [%s]", testInfo.displayName) }
     }
 
     @AfterEach
@@ -37,6 +31,17 @@ class TestLifeCycleLogger {
     }
 
     @Test
-    fun test() {
+    fun testOne() {
+        log.info { sharedVariable }
+    }
+
+    @Test
+    fun testTwo() {
+        log.info { sharedVariable }
+    }
+
+    @Test
+    fun testThree() {
+        log.info { sharedVariable }
     }
 }
