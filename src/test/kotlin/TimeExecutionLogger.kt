6c5a5ea16f072f05ac8da11f95288cc53cfd8aa3
shushistack <kjm71712@naver.com>
2023-10-18 20:16:01 +0900

    add TimeExecutionLogger

---
+++ src/test/kotlin/TimeExecutionLogger.kt
@@ -0,0 +1,7 @@
+import extensions.TimingExtension
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.extension.ExtendWith
+
+@Tag("timed")
+@ExtendWith(TimingExtension::class)
+interface TimeExecutionLogger
---
+++ src/test/kotlin/extensions/TimingExtension.kt
@@ -0,0 +1,41 @@
+package extensions
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.extension.ExtensionContext.Store
+import java.lang.reflect.Method
+
+
+class TimingExtension : BeforeTestExecutionCallback, AfterTestExecutionCallback {
+    @Throws(Exception::class)
+    override fun beforeTestExecution(context: ExtensionContext) {
+        getStore(context).put(START_TIME, System.currentTimeMillis())
+    }
+
+    @Throws(Exception::class)
+    override fun afterTestExecution(context: ExtensionContext) {
+        val testMethod: Method = context.requiredTestMethod
+        val startTime = getStore(context).remove(START_TIME, Long::class.javaPrimitiveType)
+        val duration = System.currentTimeMillis() - startTime
+
+        log.info {
+            String.format(
+                "Method [%s] took %s ms.",
+                testMethod.getName(),
+                duration
+            )
+        }
+    }
+
+    private fun getStore(context: ExtensionContext): Store {
+        return context.getStore(ExtensionContext.Namespace.create(javaClass, context.requiredTestMethod))
+    }
+
+    companion object {
+        private val log = KotlinLogging.logger {}
+
+        private const val START_TIME = "start time"
+    }
+}
