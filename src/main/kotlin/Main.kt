0722dcd66259a6385735aae8e992a24cebceb36c
shushistack <kjm71712@naver.com>
2023-10-18 20:11:43 +0900

    add TestInterfaceDynamicTestsDemo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,3 +3,8 @@ package com.sushistack
 fun main() {
     println("Hello World!")
 }
+
+fun isPalindrome(input: String): Boolean {
+    val sanitizedInput = input.replace(Regex("[^A-Za-z0-9]"), "").lowercase()
+    return sanitizedInput == sanitizedInput.reversed()
+}
---
+++ src/test/kotlin/TestInterfaceDynamicTestsDemo.kt
@@ -0,0 +1,15 @@
+
+import com.sushistack.isPalindrome
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
+import java.util.stream.Stream
+
+interface TestInterfaceDynamicTestsDemo {
+    @TestFactory
+    fun dynamicTestsForPalindromes(): Stream<DynamicTest> {
+        return Stream.of("racecar", "radar", "mom", "dad")
+            .map { text -> dynamicTest(text) { assertTrue(isPalindrome(text)) } }
+    }
+}
