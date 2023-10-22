ca9fdcdeb6fd5161abcae4e4ff3f9e4fb4ede193
shushistack <kjm71712@naver.com>
2023-10-22 20:15:51 +0900

    add Parameterized NullAndEmptySource Tests

--- src/test/kotlin/ParameterizedTestDemo.kt
+++ src/test/kotlin/ParameterizedTestDemo.kt
@@ -1,7 +1,9 @@
 import com.sushistack.isPalindrome
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullAndEmptySource
 import org.junit.jupiter.params.provider.ValueSource
+
 
 class ParameterizedTestDemo {
 
@@ -9,5 +11,13 @@ class ParameterizedTestDemo {
     @ValueSource(strings = ["racecar", "radar", "able was I ere I saw elba"])
     fun palindromes(candidate: String) {
         assertTrue(isPalindrome(candidate))
+    }
+
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = [" ", "   ", "\t", "\n"])
+    fun nullEmptyAndBlankStrings(text: String?) {
+        assertTrue(text == null || text.trim { it <= ' ' }.isEmpty())
     }
 }
