175de28e6a2195ca7d808b44de34351776f43c91
shushistack <kjm71712@naver.com>
2023-10-22 20:16:22 +0900

    add Parameterized EnumSource Test

--- src/test/kotlin/ParameterizedTestDemo.kt
+++ src/test/kotlin/ParameterizedTestDemo.kt
@@ -1,8 +1,12 @@
 import com.sushistack.isPalindrome
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 import org.junit.jupiter.params.provider.NullAndEmptySource
 import org.junit.jupiter.params.provider.ValueSource
+import java.time.temporal.ChronoUnit
+import java.time.temporal.TemporalUnit
 
 
 class ParameterizedTestDemo {
@@ -13,11 +17,16 @@ class ParameterizedTestDemo {
         assertTrue(isPalindrome(candidate))
     }
 
-
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = [" ", "   ", "\t", "\n"])
     fun nullEmptyAndBlankStrings(text: String?) {
         assertTrue(text == null || text.trim { it <= ' ' }.isEmpty())
+    }
+
+    @ParameterizedTest
+    @EnumSource(ChronoUnit::class)
+    fun testWithEnumSource(unit: TemporalUnit?) {
+        assertNotNull(unit)
     }
 }
