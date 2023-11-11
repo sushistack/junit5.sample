7c9e3862caca0ebd31370df986c2f1c3032504c7
sushistack <kjm71712@naver.com>
2023-11-11 21:35:34 +0900

    add Dynamic Test case

--- src/test/kotlin/samples/dynamic/DynamicTestsDemo.kt
+++ src/test/kotlin/samples/dynamic/DynamicTestsDemo.kt
@@ -1,10 +1,12 @@
 package samples.dynamic
 
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.TestFactory
+import java.io.File
 
 class DynamicTestsDemo {
     // 동적으로 테스트를 만듬
@@ -20,4 +22,22 @@ class DynamicTestsDemo {
     private fun isPositive(number: Int): Boolean {
         return number > 0
     }
+
+    @TestFactory
+    fun testStringLengths(): List<DynamicTest> {
+        val jsonContent = File("src/test/resources/stringLengthTestCases.json").readText()
+        val testCases: List<TestCase> = Json.decodeFromString(jsonContent)
+
+        return testCases.map { testCase ->
+            dynamicTest("String '${testCase.input}' should have length ${testCase.expectedLength}") {
+                val actualLength = testCase.input.length
+                assertEquals(testCase.expectedLength, actualLength) {
+                    "Expected length ${testCase.expectedLength} but got $actualLength"
+                }
+            }
+        }
+    }
+
+    @Serializable
+    data class TestCase(val input: String, val expectedLength: Int)
 }
